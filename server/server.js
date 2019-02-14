5260ce93add6ff9061320672050e22e7c53a6265
Your Name <you@example.com>
2019-02-14 17:43:15 +0530

    Socket IO basic setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 const publicPath = path.join(__dirname,'../public');
 console.log(__dirname+'/../public');
 console.log(publicPath);
 //console.log(config);
 const port = process.env.PORT || 5000;
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{ //Individual Socket
+    console.log('New User Connected');
+}); //Web sockets are persistant
+server.listen(port,()=>{
     console.log(`Server is up and running on ${port}`);
 });
 
