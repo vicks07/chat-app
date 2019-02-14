c529f70738deca1ebc50ae0738018d27f3a3a16d
Your Name <you@example.com>
2019-02-14 15:26:13 +0530

    Config File

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+let env = process.env.NODE_ENV || 'development';
+
+if(env === 'development' || env === 'test'){
+    let config = require('./config.json');
+    let envConfig = config[env];
+    Object.keys(envConfig).forEach((doc)=>{
+        process.env[doc] = envConfig[doc];
+    });    
+}
+
+module.exports = {
+
+}
--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT || 5000;
 console.log(__dirname+'/../public');
 console.log(publicPath);
-
+//console.log(config);
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server is up and running on ${port}`);
 });
