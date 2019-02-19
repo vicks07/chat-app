a31edb75cdfd004a1df329339d7bc6588159855d
Your Name <you@example.com>
2019-02-19 15:05:19 +0530

    Timestamping using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+//import moment = require("moment");
+
 let socket = io();
 
 socket.on('connect',function(){
@@ -27,9 +29,11 @@ socket.on('disconnect',function(){
 // });
 
 socket.on('newMessage',function(message){
+    let formattedTime = moment(message.createdAt).format('h:mm a');
+
     console.log('newMessage',message);
     let li = jQuery('<li></li>');
-    li.text(`${message.from}:${message.text}`);
+    li.text(`${message.from} ${formattedTime}:${message.text}`);
 
     jQuery('#messages').append(li);
 });
@@ -74,9 +78,10 @@ locationButton.on('click',function(){
 });
 
 socket.on('newLocationMessage',function(message){
+    let formattedTime = moment(message.createdAt).format('h:mm a');
     let li = jQuery('<li></li>');
     let a = jQuery('<a target="_blank">My Current Location</a>'); //Tells the browser to open another tab(target=_blank)
-    li.text(`${message.from}:`);
+    li.text(`${message.from} ${formattedTime}:`);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li); 
