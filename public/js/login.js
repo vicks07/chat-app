9902c03686a47967b4026f4e54f073c3230973c2
Your Name <you@example.com>
2019-03-06 17:09:15 +0530

    User Registration

--- public/js/login.js
+++ public/js/login.js
@@ -16,6 +16,50 @@ jQuery(document).ready(function () {
               });
             console.log(email,pwd);
         });
+
+        jQuery('#signup').click(function(){            
+            $('#SignUpModal').modal('show');
+      });  
+
+      jQuery('#signUpSubmit').click(function(){
+        let name = $("#signup-name").val();
+        let email = $("#signup-email").val();
+        let pwd = $("#signup-pwd").val();
+        let userDetails = {
+            name:name,
+            email:email,
+            pwd:pwd
+        }
+        console.log(userDetails);
+        // $.post(url + '/users/create', userDetails , function(result){
+        //     //$("span").html(result);
+        //     console.log(result);
+        //   });
+
+        ajaxCall('POST','/user/create',userDetails).then(function(result){
+            console.log(result);
+        })
+        //   $.ajax({
+        //     type: 'POST',
+        //     contentType: "application/json; charset=utf-8",
+        //     dataType: "json",
+        //     url: url + '/users/create',
+        //     async: async,
+        //     data: userDetails
+        //   });
+        console.log(email,pwd);
+    });
+    function ajaxCall(method,url,data){
+        //console.log('Here');
+        return  jQuery.ajax({
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            url: url,
+            async: true,
+            data: JSON.stringify(data)
+          });
+    };
+
+
 });
-
-
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const {MongoClient,ObjectId} = require('mongodb');
+const bodyParser = require('body-parser');
 
 const {generateMessage,generateLocationMessage} = require('./utils/message.js');
 const {isRealString} = require('./utils/validation.js');
@@ -21,7 +22,7 @@ const publicPath = path.join(__dirname,'../public');
 //console.log(config);
 const port = process.env.PORT;
 //console.log(process.env);
-
+app.use(bodyParser.json());
 app.use('/user',user);
 
 MongoClient.connect(process.env.MONGODB_URI,{useNewUrlParser:true},(err,client)=>{
